21764ef7da0fab5d0335a54c575936cd1e925a4d
irene-he <57462040+irene-he@users.noreply.github.com>
2020-10-17 13:26:24 -0400

    Add files via upload

---
+++ player_vs_ai.java
@@ -0,0 +1,35 @@
+package mastermind.src;
+
+import java.util.Random;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+public class player_vs_ai {
+    public static void main(String[] args) {
+        ArrayList<Integer> guesses = new ArrayList<>();
+        ai_vs_person.createAllGuesses(guesses);
+        Scanner myScanner = new Scanner(System.in);
+        String secretCode = randomCode(guesses);
+        //System.out.println(secretCode);
+        for(int guess=1; guess<=10; guess++) {
+            System.out.println("enter you guess:");
+            String input = myScanner.nextLine();
+            String answer = ai_vs_person.compare(input, secretCode);
+            System.out.println(answer);
+            if (answer.equals("40")) {
+                System.out.println("You Won!");
+                break;
+            }
+            if (guess==10){
+                System.out.println("You Lost");
+            }   
+        } 
+    }
+    public static String randomCode(ArrayList<Integer> guesses) {
+        int index = (int)(Math.random() *1296)+0;
+        String code = Integer.toString(guesses.get(index));
+        return code;
+    }
+
+}
